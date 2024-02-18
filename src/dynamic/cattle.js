86b81181a822adb6b29187ac01172e25efa02b0e
Esteban Munoz <emunoz1@harding.edu>
2024-02-18 17:01:31 -0600

    add js effect to the ForSale-btn

--- src/dynamic/cattle.js
+++ src/dynamic/cattle.js
@@ -1,6 +1,21 @@
-//hide the LeanrMore-btn
-// document.querySelectorAll('.smallBtn').forEach(btn => {
-//     btn.style.display = "none";
-// });
+//FOR SALE BTN
+let showPhoneNumber = document.querySelector(".forSale-btn");
+let phoneNumber = document.querySelector(".phone-number");
+let phoneIcon = document.querySelector(".phone-icon");
 
+showPhoneNumber.addEventListener('mouseover', mouseOver);
+showPhoneNumber.addEventListener('mouseout', mouseOut);
 
+//MOUSE OVER
+function mouseOver(){
+    phoneNumber.style.transition = "all 2s";
+    phoneNumber.innerHTML = '417-737-BEEF (2333)';
+    phoneIcon.style.display = 'none';
+}
+
+//MOUSE OUT
+function mouseOut(){
+    phoneNumber.innerHTML = 'FOR SALE';
+    phoneIcon.style.display = 'block';
+}
+  
