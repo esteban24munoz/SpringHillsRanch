e6db36c1b1b069ab4a4472da3cee0dee4fa37234
Esteban Munoz <emunoz1@harding.edu>
2024-03-11 23:23:32 -0500

    img_util and addBull require a few more changes but images are uploaded!

--- src/ranch/Ron/login/control-panel/addBull.php
+++ src/ranch/Ron/login/control-panel/addBull.php
@@ -22,6 +22,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $bullName = trim(($_POST['bullName']));
     $raaa = trim(($_POST['raaa']));
     $dob = trim(($_POST['dob']));
+    $username = trim($_SESSION['username']);
 
     do{
         if (empty($bullName) || empty($raaa) ||
@@ -36,27 +37,13 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
         $dob = htmlspecialchars($dob);
 
         // To use functions that upload and resize images
-        // require('image_util.php');
-
-        #INSERT IMG INTO DATABASE
-        // $upload_dir = 'control-panel/uploads';
-        // $upload_url = "/~emunoz1/$upload_dir";
-
-        // This is the directory the uploaded images will be placed in.
-        // It must have priviledges sufficient for the web server to write to it
-        // $upload_directory_full = "/home/emunoz1/public_html/springhillsranch/website/src/login/$upload_dir";
-        // if (!is_writeable($upload_directory_full))
-        // ShowError("The directory $upload_directory_full is not writeable.\n");
-        // exit();
-        // //GET IMG
-        // $image_filename = "$upload_directory_full/$username.jpg";
+        require('image_util.php');
 
         // Save the uploaded image to the given filename
         // $error_msg = UploadSingleImage($image_filename);
         // if ($error_msg != "")
         // ShowError($error_msg);
-        // exit();
-
+        
         // // Save uploaded image with a maximum width or height of 300 pixels
         // CreateThumbnailImage($image_filename, $image_filename, 300);
 
@@ -65,6 +52,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
         // CreateThumbnailImage($image_filename, "$upload_directory_full/$image_thumbnail", 60);
 
 
+        //INSERT VALUES INTO DB
         $sql = "INSERT INTO bulls_db (bullName, raaa, dob)" . "VALUES ('$bullName', $raaa, '$dob')";
         $result = $mysqli->query($sql);
 
@@ -84,8 +72,8 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
         $_SESSION['checkOut'] = "Check it out";
         $_SESSION['cancel'] = false;
 
-        header("location: panel.php");
-        exit;
+        // header("location: panel.php");
+        // exit;
 
     } while(false);
 
@@ -154,13 +142,16 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
                 }
                 ?>
 
+                <!-- SHOW IMG ERROR    -->
+                
+
                 <form action="addBull.php" method="POST" enctype="multipart/form-data" id='add_form'>
-                    <!-- <input type="hidden" name="MAX_FILE_SIZE" value="5000000"> -->
+                    <input type="hidden" name="MAX_FILE_SIZE" value="5000000">
 
                     <div class='row mb-3'>
-                        <label class='col-sm-3 col-form-label'>Bull Name:</label>
+                        <label class='col-sm-3 col-form-label' for='bullName'>Bull Name:</label>
                         <div class='col-sm-6'>
-                            <input class='form-control' type='text' name='bullName' value="<?php echo $bullName; ?>" autofocus required>
+                            <input class='form-control' type='text' name='bullName' id="bullName" value="<?php echo $bullName; ?>" autofocus required>
                         </div>
                     </div>
 
@@ -178,10 +169,18 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
                         </div>
                     </div>
 
+                    <!-- MAIN IMG   -->
+                    <div class='row mb-3'>
+                        <label class='col-sm-3 col-form-label' for='mainImg'>Main Image:</label>
+                        <div class='col-sm-6'>
+                            <input class='form-control' id='mainImg' type='file' name='mainImg' required>
+                        </div>
+                    </div>
+
                     <!-- SUBMIT & Cancel -->
                     <div class="row mb-3">
                         <div class="offset-sm-3 col-sm-3 d-grid">
-                            <button type="submit" class="btn btn-primary">Submit</button>
+                            <button type="submit" name='submit' class="btn btn-primary">Submit</button>
                         </div>
                         <div class="col-sm-3 d-grid">
                             <a class="btn btn-outline-primary" href='panel.php' role="button">Cancel</a>
--- src/ranch/Ron/login/control-panel/image_util.php
+++ src/ranch/Ron/login/control-panel/image_util.php
@@ -1,121 +1,57 @@
+<?php 
 
+if(isset($_POST['submit'])){
 
-<?php
+    //FILE VARIABLES
+    $file = $_FILES['mainImg'];
+    // print_r($file);
+    $fileName = $_FILES['mainImg']['name'];
+    $fileTmpName = $_FILES['mainImg']['tmp_name'];
+    $fileSize = $_FILES['mainImg']['size'];
+    $fileError = $_FILES['mainImg']['error'];
+    $fileType = $_FILES['mainImg']['type'];
 
-// image-util.php
+    $fileExt = explode('.', $fileName);
+    $fileActualExt = strtolower(end($fileExt));
 
-// This code is combined from a number of locations and modified
-// by Frank McCown, Harding University, 2009.
+    $allowed = array('jpg', 'jpeg', 'png');
 
+    // This is the directory the uploaded images will be placed in.
+    // It must have priviledges sufficient for the web server to write to it
+    $upload_dir = 'control-panel/uploads';
+    $upload_directory_full = "/home/emunoz1/public_html/springhillsranch/website/src/ranch/Ron/login/$upload_dir";
+    if (!is_writeable($upload_directory_full))
+    echo "The directory $upload_directory_full is not writeable.\n";
 
-// Returns an empty string if uploaded image is successfully saved as
-// $image_filename or an error message.
-// $image_filename should be saved in a directory that the web
-// server can write to.
-function UploadSingleImage($image_filename)
-{
-// This function is greatly modified code from
-// http://www.webdeveloper.com/forum/showthread.php?t=101466
+    //if ext is inside the array
+    if(in_array($fileActualExt, $allowed)){
+        //if no more errors
+        if($fileError === 0){
+            //check size of File
+            if($fileSize < 1000000){
 
+                //random number in time format
+                $fileNameId = uniqid('', true).".".$fileActualExt;
 
-// possible PHP upload errors
-$errors = array(1 => 'php.ini max file size exceeded (' . ini_get('upload_max_filesize') . ' limit)',
-                2 => 'html form max file size exceeded',
-                3 => 'file upload was only partial',
-                4 => 'no file was attached');
-   
-/*
-print "<pre>";
-print_r($_FILES);
-print "</pre>\n";
-*/
+                //GET IMG
+                $fileDestination = $upload_directory_full.'/'.$fileNameId;
+                move_uploaded_file($fileTmpName, $fileDestination);
+                // header('Location: panel.php?uploadsucess');
+                print "<h2>Account Created</h2>\n" .
+                "<p><img src='uploads/$fileNameId' style='float:left; margin: 0pt 10pt 10px 10px;'></p>";
+            }
+            else{
+                echo "Your file is too big!";
+            }
+        }
+        else{
+            echo "There was an error uploading your file.";
+        }
+    }
+    else{
+        echo "File extention not allowed.";
+    }
 
-// check if any files were uploaded and if
-// so store the active $_FILES array keys
-$active_keys = array();
-foreach($_FILES as $key => $file)
-{
-if (!empty($file['name']))
-{
-$active_keys[] = $key;
-}
-}
-
-// check at least one file was uploaded
-if (count($active_keys) == 0)
-return 'No files were uploaded';
-       
-// check for standard uploading errors
-foreach ($active_keys as $key)
-{
-if ($_FILES[$key]['error'] > 0)
-return $_FILES[$key]['tmp_name'] . ': ' . $errors[$_FILES[$key]['error']];
-}
-   
-// check that the file we are working on really was an HTTP upload
-foreach ($active_keys as $key)
-{
-if (!is_uploaded_file($_FILES[$key]['tmp_name']))
-return $_FILES[$key]['tmp_name'] . ' not an HTTP upload';
-}
-   
-// validation... since this is an image upload script we
-// should run a check to make sure the upload is an image
-foreach ($active_keys as $key)
-{
-if (!getimagesize($_FILES[$key]['tmp_name']))
-return $_FILES[$key]['tmp_name'].' is not an image';
-}
-   
-
-// Save every uploaded file to the same filename (normally we'd want to
-// save each file with its own unique name, but we are assuming there
-// is only one file).
-foreach ($active_keys as $key)
-{
-if (!move_uploaded_file($_FILES[$key]['tmp_name'], $image_filename))
-return 'receiving directory (' . $image_filename . ') has insuffiecient permission';
-}
-   
-// If you got this far, everything has worked and the file has been successfully saved.
-
-return '';
-}  
-
-
-// Returns true if at least one image was uploaded, false otherwise.
-function ImageUploaded()
-{
-$image_uploaded = false;
-
-$active_keys = array();
-foreach($_FILES as $key => $file)
-{
-if (!empty($file['name']))
-$image_uploaded = true;
 }
 
-return $image_uploaded;
-}
-
-// Code from https://davidwalsh.name/create-image-thumbnail-php
-function CreateThumbnailImage($src_filename, $dest_filename, $desired_width)
-{
-$source_image = imagecreatefromjpeg($src_filename);
-$width = imagesx($source_image);
-$height = imagesy($source_image);
-
-// create height based on desired width, keeping the same aspect ratio
-$desired_height = floor($height * ($desired_width / $width));
-
-// create a new, "virtual" image
-$virtual_image = imagecreatetruecolor($desired_width, $desired_height);
-
-// copy source image at a resized size
-imagecopyresampled($virtual_image, $source_image, 0, 0, 0, 0, $desired_width,
-$desired_height, $width, $height);
-
-// create the physical thumbnail image to its destination
-imagejpeg($virtual_image, $dest_filename);
-}
 ?>
